a240166bb0f9ab51b1bab8d941b42f2f4fe95f56
kdcc-005 <ray.luo@kangdainfo.com>
2021-05-24 19:58:41 +0800

    update

--- src/main/java/indi/rennnhong/staterkit/module/drawlots/controller/DrawLotsApiController.java
+++ src/main/java/indi/rennnhong/staterkit/module/drawlots/controller/DrawLotsApiController.java
@@ -29,30 +29,25 @@ public class DrawLotsApiController {
     }
 
     private String getBase64NumberImage(Integer number) {
-        File file = null;
-        try {
-            file = ResourceUtils.getFile(MessageFormat.format("classpath:static/img/number_{0}.png",Integer.toString(number)));
 
 //        String imgFile = MessageFormat.format("static/img/number_{0}.png", Integer.toString(number));//待處理的圖片
-            InputStream in = null;
-            byte[] data = null;
-            //讀取圖片位元組陣列
-            try {
-                in = new FileInputStream(file);
-                data = new byte[in.available()];
-                in.read(data);
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //對位元組陣列Base64編碼
-            BASE64Encoder encoder = new BASE64Encoder();
-            String encode = encoder.encode(data);
-            System.out.println(encode);
-            return encode;//返回Base64編碼過的位元組陣列字串
-        } catch (FileNotFoundException e) {
+        InputStream in = null;
+        byte[] data = null;
+        //讀取圖片位元組陣列
+        try {
+//                in = new FileInputStream(file);
+            in = ResourceUtils.getURL(MessageFormat.format("classpath:static/img/number_{0}.png", Integer.toString(number))).openStream();
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        //對位元組陣列Base64編碼
+        BASE64Encoder encoder = new BASE64Encoder();
+        String encode = encoder.encode(data);
+        System.out.println(encode);
+        return encode;//返回Base64編碼過的位元組陣列字串
     }
 }
+
